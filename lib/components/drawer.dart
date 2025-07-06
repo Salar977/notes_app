2d1de49607b99d3c9449dba9b1b6283646b787b3
Salar Amir <salar831@gmail.com>
2025-07-06 23:58:35 +0200

    Add SettingsPage and integrate it into MyDrawer for navigation

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/components/drawer_tile.dart';
+import 'package:notes_app/pages/settings_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -21,7 +22,22 @@ class MyDrawer extends StatelessWidget {
             title: "Notes",
             leading: const Icon(Icons.home),
             onTap: () => Navigator.pop(context),
-          )
+          ),
+
+          // settings tile
+          DrawerTile(
+            title: "Settings",
+            leading: const Icon(Icons.settings),
+            onTap: () {
+              // pop
+              Navigator.pop(context);
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage())
+              );
+            }
+          ),
 
         ],
       ),
