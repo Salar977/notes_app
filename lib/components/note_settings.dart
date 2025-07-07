371719da7bdc8243d1540319baa76963c6bd59e1
Salar Amir <salar831@gmail.com>
2025-07-07 21:04:16 +0200

    Change edit and delete button layout to popover

---
+++ lib/components/note_settings.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class NoteSettings extends StatelessWidget {
+  final void Function()? onEditTap;
+  final void Function()? onDeleteTap;
+
+  const NoteSettings({
+    super.key,
+    required this.onEditTap,
+    required this.onDeleteTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Edit button
+        GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+            onEditTap!();
+          },
+          child: Container(
+            height: 50,
+            color: Theme.of(context).colorScheme.surface,
+            child: Center(
+              child: Text(
+                "Edit",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        // Delete Button
+        GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+            onDeleteTap!();
+          },
+          child: Container(
+            height: 50,
+            color: Theme.of(context).colorScheme.surface,
+            child: Center(
+              child: Text(
+                "Delete",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/components/note_tile.dart
+++ lib/components/note_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/components/note_settings.dart';
+import 'package:popover/popover.dart';
 
 class NoteTile extends StatelessWidget {
   final String text;
@@ -27,21 +29,22 @@ class NoteTile extends StatelessWidget {
             fontSize: 18,
           ),
         ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // edit button
-            IconButton(
-              onPressed: onEditPressed,
-              icon: const Icon(Icons.edit)
-            ),
-
-            // delete button
-            IconButton(
-              onPressed: onDeletePressed,
-              icon: const Icon(Icons.delete)
-            ),
-          ],
+        trailing: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () => showPopover(
+                width: 100,
+                height: 100,
+                backgroundColor: Theme.of(context).colorScheme.surface,
+                context: context,
+                bodyBuilder: (context) => NoteSettings(
+                  onEditTap: onEditPressed,
+                  onDeleteTap: onDeletePressed,
+                ),
+              ),
+            );
+          }
         ),
       ),
     );
